b461d960f5ed47f2c823a60bea263a55092b9d1d
sashaTribe <sasha555.tribe@gmail.com>
2023-10-31 17:54:24 +0000

    successfully created my database

--- connect_db.py
+++ connect_db.py
@@ -1,16 +1,25 @@
 import sqlite3, csv
 from create_datasets import FILE_NAMES
 
-con = sqlite3.connect("pizza_chain.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE customer_reviews (id, title, review, date, ratings)")
+try:
+    con = sqlite3.connect("pizza_chain.db")
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS Customer_reviews")
+    cur.execute("CREATE TABLE Customer_reviews (title, review, date, ratings)")
 
-for file in FILE_NAMES:
-    with open(file, 'r') as f:
-        dr = csv.DictReader(f)
-        to_db = [(i[''], i['title'], i['review'], i['date'], i['ratings']) for i in dr]
+    for file in FILE_NAMES:
+        with open(file, 'r', encoding= 'utf-8') as f:
+            dr = csv.DictReader(f)
+            to_db = [(i['Title of Review'], i['Review Description'], i['Date of Review'], i['No. of stars']) for i in dr]
 
-    cur.executemany("INSERT INTO customer_reviews (id, title, review, date, ratings VALUES (?,?,?,?,?);",to_db)
-    con.commit()
+        cur.executemany("INSERT INTO customer_reviews (title, review, date, ratings) VALUES (?,?,?,?);",to_db)
+        con.commit()
+        print(file, " uploaded to db")
     con.close()
+except sqlite3.Error as error:
+    print("Error while creating Customer Reviews Table", error)
+finally:
+    if con:
+        con.close()
+        print("Connection now closed")
 
