d0a6265435a29f221e02d0e2a105e20a85cdf10b
sashaTribe <sasha555.tribe@gmail.com>
2023-10-31 16:34:31 +0000

    Created a new file gets datasets for every chain

---
+++ get_datasets.py
@@ -0,0 +1,6 @@
+from fetch_data import fetch_from_trustpilot, create_data_frame
+import pandas as pd
+import csv
+
+list_of_chains = ['www.prezzorestaurants.com','www.zizzi.co.uk','francomanca.co.uk']
+
