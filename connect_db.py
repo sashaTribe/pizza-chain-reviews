1e810b720f61af81a8c245636cfc55f91e5827f1
sashaTribe <sasha555.tribe@gmail.com>
2023-10-31 17:37:53 +0000

    added functions here

--- connect_db.py
+++ connect_db.py
@@ -1,3 +1,16 @@
 import sqlite3, csv
+from create_datasets import FILE_NAMES
 
 con = sqlite3.connect("pizza_chain.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE customer_reviews (id, title, review, date, ratings)")
+
+for file in FILE_NAMES:
+    with open(file, 'r') as f:
+        dr = csv.DictReader(f)
+        to_db = [(i[''], i['title'], i['review'], i['date'], i['ratings']) for i in dr]
+
+    cur.executemany("INSERT INTO customer_reviews (id, title, review, date, ratings VALUES (?,?,?,?,?);",to_db)
+    con.commit()
+    con.close()
+
